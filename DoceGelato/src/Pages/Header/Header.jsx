892e7d2f87df9dab3dfdc0ea37213169e6a3739a
Thyale MD <thyaledantas@gmail.com>
2025-01-30 11:56:14 -0300

    Conteudo do header sem o css

--- DoceGelato/src/Pages/Header/Header.jsx
+++ DoceGelato/src/Pages/Header/Header.jsx
@@ -20,7 +20,18 @@ export default function Header(){
             </div>
 
             <div className="BarraMenu">
-                <p>Barramenu</p>
+                <img src="/src/assets/LogoDoceGelato.png" alt="Logotipo do DoceGelato" />
+                <ul className="BarraMenu_Links">
+                    <li><a href="#">Milkshake</a></li>
+                    <li><a href="#">Picolé</a></li>
+                    <li><a href="#">Sorvete</a></li>
+                </ul>
+                <div className="BarraMenu_Pesquisa" aria-label="Campo de pesquisa">
+                    <input type="text" name="Pesquisa" id="Pesquisa" />
+                    <label htmlFor="Pesquisa">
+                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16"><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/></svg>
+                    </label>
+                </div>
             </div>
 
         </header>
