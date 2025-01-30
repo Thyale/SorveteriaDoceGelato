3c41ba98d15d4fc08df19ba16c5f3340d0a2fe2d
Thyale MD <thyaledantas@gmail.com>
2025-01-30 12:25:41 -0300

    Header finalizado

--- DoceGelato/src/Pages/Header/Header.jsx
+++ DoceGelato/src/Pages/Header/Header.jsx
@@ -27,7 +27,7 @@ export default function Header(){
                     <li><a href="#">Sorvete</a></li>
                 </ul>
                 <div className="BarraMenu_Pesquisa" aria-label="Campo de pesquisa">
-                    <input type="text" name="Pesquisa" id="Pesquisa" />
+                    <input placeholder="PESQUISE POR ALGO" type="text" name="Pesquisa" id="Pesquisa" />
                     <label htmlFor="Pesquisa">
                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16"><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/></svg>
                     </label>
