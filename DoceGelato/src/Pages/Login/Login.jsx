7708149150dddcef632d7677d7f4cf260ab1aaa1
Thyale MD <thyaledantas@gmail.com>
2025-02-05 10:11:36 -0300

    Registro e login Introdução
    Já coloquei toda a interação, falta colocar as mensagens de forma correta e mudar a foto ao realizar o login

--- DoceGelato/src/Pages/Login/Login.jsx
+++ DoceGelato/src/Pages/Login/Login.jsx
@@ -1,4 +1,20 @@
+import { useState } from "react"
+
 export default function Login(){
+
+    //Variáveis que vao ver o que foi digitado no login
+    const [emailLogin, setEmailLogin] = useState("")
+    const [senhaLogin, setSenhaLogin] = useState("")
+
+    //Aqui eu vou fazer um if e um else, se o login for igual aos dados cadastrados no usuario registrado ele entra, se não for ele da a mensagem de que precisa ser feito o registro.
+    function VerificarLoginUser(){
+        let usuarioRecuperado = JSON.parse(localStorage.getItem("Usuario"));
+        if(usuarioRecuperado.emailUsuario === emailLogin & usuarioRecuperado.senhaUsuario === senhaLogin){
+            alert("Usuaio reconheido e entrando")
+        }else{
+            alert("não possiu registro, faça agora")
+        }
+    }
 
     return(
         <section className="Login">
@@ -6,11 +22,11 @@ export default function Login(){
                <h1>Login</h1>
                <form action="">
                    <label htmlFor="email">Email</label>
-                   <input className="InputUser" type="email" name="email" id="email" />
+                   <input onChange={(emaildigitado) => setEmailLogin(emaildigitado.target.value)} className="InputUser" type="email" name="email" id="email" />
                    <label htmlFor="senha">Senha</label>
-                   <input className="InputUser" type="password" name="senha" id="senha" />
+                   <input onChange={(senhadigitada) => setSenhaLogin(senhadigitada.target.value)} className="InputUser" type="password" name="senha" id="senha" />
                    <p className="Text_EsqueceuSenha">Esqueceu a senha? <a href="#">Clique aqui</a></p>
-                   <input className="Btn_login" type="button" value="Login" />
+                   <input onClick={VerificarLoginUser} className="Btn_login" type="button" value="Login" />
                </form>
                <p className="Text_Registro">Não tem cadastro? <a href={"/Register"}>Registre-se</a></p>
             </div>
--- DoceGelato/src/Pages/Register/Register.jsx
+++ DoceGelato/src/Pages/Register/Register.jsx
@@ -1,16 +1,49 @@
+import { useState } from "react";
+
 export default function Register() {
+
+  //Criando o objeto CENTRAL de Usuário, vazio e salvamos no localStorage
+  const Usuario = {
+    nomeUsuario: "",
+    emailUsuario: "",
+    senhaUsuario: "",
+  }
+  localStorage.setItem("Usuario", JSON.stringify(Usuario));
+
+  //Criei as variáveis pra salvar o que foi escrito no input
+  const [emailRegistro, setEmailRegistro] = useState("")
+  const [senhaRegistro, setSenhaRegistro] = useState("")
+  const [nomeRegistro, setNomeegistro] = useState("")
+
+  // ao clicar em salvar, vamos verificar se os campos foram escritos, se forem nos pegamos o objeto que estava vazio e colocamos as novas informações escritas nos inputs e atualizamos ele, e se nao tiver aparece a mensagem de preencher tudo. 
+  function SalvandoUsuarioRegistrado() {
+
+    if (!emailRegistro || !senhaRegistro || !nomeRegistro) {
+      alert("Preencha todos os campos!");
+    } else {
+      let usuarioRecuperado = JSON.parse(localStorage.getItem("Usuario"));
+
+      usuarioRecuperado.nomeUsuario = nomeRegistro;
+      usuarioRecuperado.emailUsuario = emailRegistro;
+      usuarioRecuperado.senhaUsuario = senhaRegistro;
+
+      localStorage.setItem("Usuario", JSON.stringify(usuarioRecuperado));
+      alert("Usuário cadastrado com sucesso! Volte e faça o login.");
+    }    
+  }  
+
   return (
     <section className="Register">
       <div className="Register_ContainerForm">
         <h1>Registro</h1>
         <form action="">
           <label htmlFor="user">User</label>
-          <input className="InputRegister" type="text" name="user" id="user" />
+          <input onChange={(nomeregistrado) => setNomeegistro(nomeregistrado.target.value)} className="InputRegister" type="text" name="user" id="user" />
           <label htmlFor="email">Email</label>
-          <input className="InputRegister" type="email" name="email" id="email" />
+          <input onChange={(emailregistrado) => setEmailRegistro(emailregistrado.target.value)} className="InputRegister" type="email" name="email" id="email" />
           <label htmlFor="senha">Senha</label>
-          <input className="InputRegister" type="password" name="senha" id="senha" />
-          <input className="Btn_Registro" type="button" value="Registre-se" />
+          <input onChange={(senharegistrada)=>setSenhaRegistro(senharegistrada.target.value)} className="InputRegister" type="password" name="senha" id="senha" />
+          <input onClick={SalvandoUsuarioRegistrado} className="Btn_Registro" type="button" value="Registre-se" />
         </form>
         <p className="Text_Login">Já tem cadastro? <a href={"/Login"}>Login</a></p>
       </div>
